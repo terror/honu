3c4d142835ad6b46f217decb84e3d9bde0e675b9
liam <liam@scalzulli.com>
2026-08-11 01:12:34 -0400

    Use lowercase shell names when importing (#77)

--- src/shell/fish.rs
+++ src/shell/fish.rs
@@ -7,4 +7,3 @@ mod parser;
 pub(super) const DEFAULT_HISTORY_FILE: &str = ".local/share/fish/fish_history";
 pub(super) const FORMAT: &str = "fish";
 pub(super) const INIT: &str = include_str!("fish/init.fish");
-pub(super) const NAME: &str = "Fish";
--- src/shell/zsh.rs
+++ src/shell/zsh.rs
@@ -8,4 +8,3 @@ mod parser;
 pub(super) const DEFAULT_HISTORY_FILE: &str = ".zsh_history";
 pub(super) const FORMAT: &str = "zsh";
 pub(super) const INIT: &str = include_str!("zsh/init.zsh");
-pub(super) const NAME: &str = "Zsh";
